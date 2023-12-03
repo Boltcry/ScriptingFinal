df701eb15f7aca5026697fe51597b5fe47262ce8
Boltcry <crystal.hicks@utexas.edu>
2023-12-03 16:50:58 -0600

    Revert "Merge branch 'miracc47-pathfindingscripts'"
    This reverts commit be03bce497b13324842655aebf655f1cd5ce9357, reversing
    changes made to 3e43922a583a25e91fd104325ba8ec9e8a50d209.

--- Assets/Scripts/PlayerPathFinder.cs
+++ Assets/Scripts/PlayerPathFinder.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerPathFinder : MonoBehaviour
 {
+
     public float speed = 10f;
     private Vector3 targetPosition;
 
@@ -16,52 +17,20 @@ public class PlayerPathFinder : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
     public IEnumerator MoveToTarget(Vector2 target)
     {
-        // Assume there is a PathfindingNode script attached to the GameObject
-        PathfindingNode[] allNodes = GameObject.FindObjectsOfType<PathfindingNode>();
-        PathfindingNode startNode = FindNearestNode(transform.position, allNodes);
-        PathfindingNode targetNode = FindNearestNode(target, allNodes);
 
-        if (startNode != null && targetNode != null)
+        targetPosition = new Vector3(target.x, target.y, 0.0f);
+
+        while (Vector3.Distance(transform.position, targetPosition) > 0.01f)
         {
-            List<PathfindingNode> path = startNode.DepthFirstSearch(startNode, targetNode);
-
-            if (path != null)
-            {
-                foreach (PathfindingNode node in path)
-                {
-                    targetPosition = new Vector3(node.transform.position.x, node.transform.position.y, 0.0f);
-
-                    while (Vector3.Distance(transform.position, targetPosition) > 0.01f)
-                    {
-                        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
-                        yield return null;
-                    }
-                }
-            }
-        }
-    }
-
-    private PathfindingNode FindNearestNode(Vector2 position, PathfindingNode[] nodes)
-    {
-        float minDistance = float.MaxValue;
-        PathfindingNode nearestNode = null;
-
-        foreach (PathfindingNode node in nodes)
-        {
-            float distance = Vector2.Distance(position, new Vector2(node.transform.position.x, node.transform.position.y));
-
-            if (distance < minDistance)
-            {
-                minDistance = distance;
-                nearestNode = node;
-            }
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+            yield return null;
         }
 
-        return nearestNode;
+        transform.position = targetPosition;
     }
 }
